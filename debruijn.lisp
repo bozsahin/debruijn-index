813f8a127a485f3144b30921b5a25f971d9272b9
https://github.com/bozsahin <cem.bozsahin@gmail.com>
2021-04-07 21:54:54 +0300

    double recurse on body

--- debruijn.lisp
+++ debruijn.lisp
@@ -8,19 +8,30 @@
   `(and (listp ,lterm) (= (length ,lterm) 2)))  ; complex bodies are assumed binarized
 
 (defmacro unarybp (lterm)
-  `(or (atom ,lterm) (and (listp ,lterm) (= (length ,lterm) 1))))
+  `(or (atom ,lterm) (and (listp ,lterm) (= (length ,lterm) 1)))) ; simple bodies may include lambdas
 
-(defmacro l-get-body (lterm)
+(defmacro l-get-b (lterm)
   `(third ,lterm))
 
-(defun debruijn (lterm &optional (dterm nil) (bl nil))
-  "build the debruijn term dterm from lambda term lterm using binding list bl"
-  (cond ((null lterm) (values dterm bl))
-	((bindp lterm) (debruijn (l-get-body lterm) 
-				 (mk-dterm-bind lterm dterm) (mk-bl lterm bl)))
-	((binarybp lterm) (debruijn (l-get-body21 lterm ) 
-				    (mk-dterm-bin lterm dterm) bl))
+(defmacro l-get-v (lterm)
+  `(second ,lterm))
+
+(defun debruijn (lterm &optional (dterm nil) (bl nil) (l 0))
+  "build the l level debruijn term dterm 
+  from lambda term lterm using binding list bl"
+  (cond ((null lterm) dterm)
+	((bindp lterm) (debruijn (l-get-b lterm) 
+				 (mk-dterm-bind lterm dterm) 
+				 (mk-bl (l-get-v lterm) bl l)
+				 (+ l 1)))
+	((binarybp lterm) (debruijn (l-get-b21 lterm) 
+				    (mk-dterm-bin lterm dterm) 
+				    bl l)
+					(debruijn (l-get-b22 lterm) 
+						  (mk-dterm-bin lterm dterm) 
+						  bl l)
 	((unarybp  lterm) (debruijn (l-get-body1 lterm)
-			            (mk-dterm-un  lterm dterm) bl))
+			            (mk-dterm-un  lterm dterm) 
+				    bl l))
 	(t (debruijn nil))))
 
