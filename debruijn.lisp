a525783c79043c5b64af2c0339a09dc1c5e458e9
https://github.com/bozsahin <cem.bozsahin@gmail.com>
2021-04-09 19:40:02 +0300

    simplified unary bodies

--- debruijn.lisp
+++ debruijn.lisp
@@ -10,28 +10,48 @@
 (defmacro unarybp (lterm)
   `(or (atom ,lterm) (and (listp ,lterm) (= (length ,lterm) 1)))) ; simple bodies may include lambdas
 
-(defmacro l-get-b (lterm)
+(defmacro l-get-bb (lterm)
   `(third ,lterm))
 
-(defmacro l-get-v (lterm)
+(defmacro l-get-vv (lterm)
   `(second ,lterm))
+
+(defmacro mk-bl (v bl lev)
+  `(if (null ,bl) 
+     (cons (list ,v (+ ,lev 1))  ,bl) 
+     (list (list ,v (+ ,lev 1))  ,bl)))
+
+(defmacro mk-dterm-bind (dterm)
+  `(list 'LAM ,dterm))
+
+(defmacro mk-dterm-bin (d1 d2)
+  `(list ,d1 ,d2))
+
+(defmacro l-get-b21 (lterm)
+  `(first ,lterm))
+
+(defmacro l-get-b22 (lterm)
+  `(second ,lterm))
+
+(defmacro debr-index (lterm bl)
+  `(second (assoc ,lterm ,bl)))
+
+(defmacro free (lterm bl)
+  `(null (assoc ,lterm ,bl))) 
+
+(defmacro constantbp (lterm bl)
+  `(or (constantp ,lterm) (free ,lterm ,bl)))
 
 (defun debruijn (lterm &optional (dterm nil) (bl nil) (l 0))
   "build the l level debruijn term dterm 
   from lambda term lterm using binding list bl"
   (cond ((null lterm) dterm)
-	((bindp lterm) (debruijn (l-get-b lterm) 
-				 (mk-dterm-bind lterm dterm) 
-				 (mk-bl (l-get-v lterm) bl l)
+	((bindp lterm) (debruijn (l-get-bb lterm) 
+				 (mk-dterm-bind  dterm) 
+				 (mk-bl (l-get-vv lterm) bl l)
 				 (+ l 1)))
-	((binarybp lterm) (debruijn (l-get-b21 lterm) 
-				    (mk-dterm-bin lterm dterm) 
-				    bl l)
-					(debruijn (l-get-b22 lterm) 
-						  (mk-dterm-bin lterm dterm) 
-						  bl l)
-	((unarybp  lterm) (debruijn (l-get-body1 lterm)
-			            (mk-dterm-un  lterm dterm) 
-				    bl l))
-	(t (debruijn nil))))
+	((binarybp lterm) (mk-dterm-bin (debruijn (l-get-b21 lterm) nil bl 0)
+					(debruijn (l-get-b22 lterm) nil bl 0)))
+	((constantbp  lterm bl) lterm)
+	(t (debr-index lterm bl))))
 
