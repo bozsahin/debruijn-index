0c0c64f8b7ba810e68332c6814a755f38847f638
Cem Bozsahin <cem.bozsahin@gmail.com>
2021-04-23 18:21:49 +0300

    recursion avoids dterm collect

--- debruijn.lisp
+++ debruijn.lisp
@@ -22,10 +22,18 @@
      (list (list ,v (+ ,lev 1))  ,bl)))
 
 (defmacro mk-dterm-bind (dterm)
-  `(list 'LAM ,dterm))
+  `(if (null ,dterm)
+     (cons 'LAM ,dterm)
+     (list 'LAM ,dterm)))
 
 (defmacro mk-dterm-bin (d1 d2)
   `(list ,d1 ,d2))
+
+(defmacro mk-dterm-const (term)
+  `(identity ,term))
+
+(defmacro mk-dterm-index (lterm bl)
+  `(second (assoc ,lterm ,bl)))
 
 (defmacro l-get-b21 (lterm)
   `(first ,lterm))
@@ -33,25 +41,21 @@
 (defmacro l-get-b22 (lterm)
   `(second ,lterm))
 
-(defmacro debr-index (lterm bl)
-  `(second (assoc ,lterm ,bl)))
-
 (defmacro free (lterm bl)
   `(null (assoc ,lterm ,bl))) 
 
 (defmacro constantbp (lterm bl)
   `(or (constantp ,lterm) (free ,lterm ,bl)))
 
-(defun debruijn (lterm &optional (dterm nil) (bl nil) (l 0))
-  "build the l level debruijn term dterm 
+(defun debruijn (lterm &optional (bl nil) (l 0))
+  "build the l level debruijn term 
   from lambda term lterm using binding list bl"
-  (cond ((null lterm) dterm)
-	((bindp lterm) (debruijn (l-get-bb lterm) 
-				 (mk-dterm-bind  dterm) 
-				 (mk-bl (l-get-vv lterm) bl l)
-				 (+ l 1)))
-	((binarybp lterm) (mk-dterm-bin (debruijn (l-get-b21 lterm) nil bl 0)
-					(debruijn (l-get-b22 lterm) nil bl 0)))
-	((constantbp  lterm bl) lterm)
-	(t (debr-index lterm bl))))
-
+  (cond ((null lterm)  lterm)
+	((bindp lterm) (mk-dterm-bind       ; (LAM V B)
+			 (debruijn (l-get-bb lterm)
+				   (mk-bl (l-get-vv lterm) bl l)
+				   (+ l 1))))
+	((binarybp lterm) (mk-dterm-bin (debruijn (l-get-b21 lterm)  bl 0) ; (E1 E2)
+					(debruijn (l-get-b22 lterm)  bl 0))) 
+	((constantbp  lterm bl) (mk-dterm-const lterm))                                        ; E
+	(t (mk-dterm-index lterm bl))))  ; must be bound variable if all above  fails
